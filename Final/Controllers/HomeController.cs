41e7245803d27aad58c707a6770309773de74e2c
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2024-11-14 02:12:56 +0400

    front-to-back

--- Final/Controllers/HomeController.cs
+++ Final/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace Final.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
     }
 }
