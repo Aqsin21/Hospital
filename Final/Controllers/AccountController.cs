412988e1d4fdc28e3be5ef0f26b88fe035a56305
Aqsin21 <150579577+Aqsin21@users.noreply.github.com>
2024-11-27 01:05:39 +0400

    Reg 1

---
+++ Final/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Final.Data.DAL;
+using Final.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages;
+
+namespace Final.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        public AccountController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(Register model)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                if (_context.Users.Any(u => u.UserName == model.UserName))
+                {
+                    ModelState.AddModelError("Username", "Bu kullanıcı adı zaten alınmış.");
+                    return View(model);
+                }
+
+
+                var hashedPassword = BCrypt.Net.BCrypt.HashPassword(model.Password);
+
+
+
+
+                _context.Users.Add(model);
+                _context.SaveChanges();
+
+
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Final/Models/Register.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Final.Models
+{
+    public class Register
+    {
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "Username is required")]
+        public string UserName { get; set; }
+
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Email is invalid")]
+        public string Email { get; set; }
+
+
+        public string PasswordHash { get; set; }
+    }
+}
